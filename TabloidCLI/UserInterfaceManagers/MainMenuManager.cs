1ab9058ea579c5a53bae3c9bd947c200d1f99c37
Joshua Johns <joshuajohns28@outlook.com>
2022-12-14 15:58:19 -0600

    finished adding Journal Entry feature.

--- TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
+++ TabloidCLI/UserInterfaceManagers/MainMenuManager.cs
@@ -5,7 +5,7 @@ namespace TabloidCLI.UserInterfaceManagers
     public class MainMenuManager : IUserInterfaceManager
     {
         private const string CONNECTION_STRING = 
-            @"Data Source=localhost\SQLEXPRESS;Database=TabloidCLI;Integrated Security=True";
+            @"Data Source=localhost\SQLEXPRESS01;Database=TabloidCLI;Integrated Security=True";
 
         public IUserInterfaceManager Execute()
         {
@@ -23,7 +23,7 @@ namespace TabloidCLI.UserInterfaceManagers
             string choice = Console.ReadLine();
             switch (choice)
             {
-                case "1": throw new NotImplementedException();
+                case "1": return new JournalManager(this, CONNECTION_STRING);
                 case "2": throw new NotImplementedException();
                 case "3": return new AuthorManager(this, CONNECTION_STRING);
                 case "4": throw new NotImplementedException();
