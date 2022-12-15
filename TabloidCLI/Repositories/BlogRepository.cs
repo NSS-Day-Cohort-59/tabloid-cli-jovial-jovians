da689361eeee19659221932014dfa6a8caccad89
Sarah Beth Langley <sarahbethdoescode@gmail.com>
2022-12-15 09:20:35 -0600

    created blog manager and repository

---
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+
+namespace TabloidCLI.Repositories
+{
+    public class BlogRepository : DatabaseConnector, IRepository<Blog>
+    {
+        public BlogRepository(string connectionString) : base(connectionString) { }
+
+        public List<Blog> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+        public Blog Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+        public void Insert(Blog blog)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Blog (Title, URL )
+                                                     VALUES (@blogTitle, @URL)";
+                    cmd.Parameters.AddWithValue("@blogTitle", blog.Title);
+                    cmd.Parameters.AddWithValue("@URL", blog.Url);
+                    
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+       
+        public void Update(Blog entry)
+        {
+            throw new NotImplementedException();
+        }
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+       
+    }
+}
